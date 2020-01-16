c5b6731bc46bcbf10da4671b5781ba686611cb54
Peter Paul Elfferich <pp@exmachina.nl>
2020-01-16 17:13:01 +0100

    Removed unnecessary example parts.

--- components/livery-demo/test/livery-demo.test.js
+++ components/livery-demo/test/livery-demo.test.js
@@ -3,47 +3,6 @@ import { html, fixture, expect } from '@open-wc/testing';
 import '../livery-demo.js';
 
 describe('LiveryDemo', () => {
-  it('has page "main" by default', async () => {
-    const el = await fixture(html`
-      <livery-demo></livery-demo>
-    `);
-
-    expect(el.page).to.equal('main');
-    expect(el.shadowRoot.querySelector('main')).lightDom.to.equal(`
-      <page-main></page-main>
-    `);
-  });
-
-  it('renders default fallback content', async () => {
-    const el = await fixture(html`
-      <livery-demo></livery-demo>
-    `);
-    el.page = undefined;
-
-    expect(el.page).to.equal(undefined);
-    expect(el.shadowRoot.querySelector('main')).lightDom.to.equal(`
-      <page-main></page-main>
-    `);
-  });
-
-  it('renders page-one if page property is set to pageOne', async () => {
-    const el = await fixture(html`
-      <livery-demo page="pageOne"></livery-demo>
-    `);
-    expect(el.shadowRoot.querySelector('main')).lightDom.to.equal(`
-      <page-one></page-one>
-    `);
-  });
-
-  it('changes the page if a menu link gets clicked', async () => {
-    const el = await fixture(html`
-      <livery-demo></livery-demo>
-    `);
-    el.shadowRoot.querySelectorAll('header a')[2].click();
-
-    expect(el.page).to.equal('about');
-  });
-
   it('matches the snapshot', async () => {
     const el = await fixture(html`
       <livery-demo></livery-demo>

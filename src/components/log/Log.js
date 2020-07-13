f93a954208ee554aaecdd7b3e3d1231255040366
frits-exmachina <frits@exmachina.nl>
2020-07-13 16:36:28 +0200

    Added amplify.yml

--- src/components/log/Log.js
+++ src/components/log/Log.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import "./Log.css";
 
+
+function LevelSelect(props){
+
+    return(
+        <div className="level-button">
+            <span>Level</span>
+        </div>
+    );
+
+}
+
 class Log extends Component {
 
     
@@ -8,6 +19,7 @@ class Log extends Component {
     render() {
         return (
             <div className="Log">
+                <LevelSelect></LevelSelect>
                 <div className="log-box">
              
                     <livery-log></livery-log>

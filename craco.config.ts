bda32db0ecb8e8c7f523089b9eb53d067e300900
Oleksandr S <oleksandr.slukovskyi@gmail.com>
2022-05-11 23:09:42 +0200

    Migration to typescript

---
+++ craco.config.ts
@@ -0,0 +1,15 @@
+const CracoAlias = require('craco-alias')
+
+const config = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        tsConfigPath: 'tsconfig.paths.json',
+      },
+    },
+  ],
+}
+
+export default config

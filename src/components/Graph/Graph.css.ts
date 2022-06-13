e0fb87750e4331dc9bfa063d5369617d5ca4bad1
Oleksandr Slukovskyi <oleksandr.slukovskyi@gmail.com>
2022-06-13 13:57:02 +0200

    updated positions of blocks in graph sections on mobile devices

--- src/components/Graph/Graph.css.ts
+++ src/components/Graph/Graph.css.ts
@@ -2,18 +2,43 @@ import { style } from '@vanilla-extract/css'
 
 export const container = style({
   display: 'flex',
+  flexDirection: 'column',
+  gridGap: '15px',
+  '@media': {
+    '(min-width: 992px)': {
+      gridGap: 0,
+      alignItems: 'top',
+      flexDirection: 'row',
+    },
+  },
 })
 
 export const chipsContainer = style({
   display: 'flex',
-  flexDirection: 'column',
+  flexDirection: 'row',
   gridGap: '15px',
+  justifyContent: 'center',
+  order: 2,
+  '@media': {
+    '(min-width: 992px)': {
+      justifyContent: 'flex-start',
+      flexDirection: 'column',
+      order: 1,
+    },
+  },
 })
 
 export const graphContainer = style({
   position: 'relative',
   overflow: 'hidden',
   width: '100%',
+  minHeight: '200px',
+  order: 1,
+  '@media': {
+    '(min-width: 992px)': {
+      order: 2,
+    },
+  },
 })
 
 export const graph = style({
